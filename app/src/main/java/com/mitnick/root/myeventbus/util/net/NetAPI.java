7c4a86fd1d0b18a3396db8e70d2238469ba79305
root <897922356@qq.com>
2016-07-08 23:21:39 +0800

    修改网络请求工厂

--- app/src/main/java/com/mitnick/root/myeventbus/util/net/NetAPI.java
+++ app/src/main/java/com/mitnick/root/myeventbus/util/net/NetAPI.java
@@ -1,8 +1,9 @@
 package com.mitnick.root.myeventbus.util.net;
 
-import retrofit.Response;
-import retrofit.http.GET;
-import retrofit.http.Query;
+
+import retrofit2.Response;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
 import rx.Observable;
 
 /**
--- app/src/main/java/com/mitnick/root/myeventbus/util/net/NetAPIFactory.java
+++ app/src/main/java/com/mitnick/root/myeventbus/util/net/NetAPIFactory.java
@@ -4,9 +4,19 @@ package com.mitnick.root.myeventbus.util.net;
  * Created by root on 16-6-10.
  */
 
-import retrofit.GsonConverterFactory;
-import retrofit.Retrofit;
-import retrofit.RxJavaCallAdapterFactory;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Headers;
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 
 public class NetAPIFactory {
 
@@ -15,15 +25,65 @@ public class NetAPIFactory {
 
     public static void netAPIInit(String base_server_ip ){
         if(null == retrofit){
+
+            OkHttpClient.Builder builder = new OkHttpClient.Builder();
+            //设置超时
+            builder.connectTimeout(15, TimeUnit.SECONDS);
+            builder.readTimeout(20, TimeUnit.SECONDS);
+            builder.writeTimeout(20, TimeUnit.SECONDS);
+            //错误重连
+            builder.retryOnConnectionFailure(true);
+            setInterceptor(builder);
+
             retrofit = new Retrofit.Builder()
                     .baseUrl(base_server_ip)
                     .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(builder.build())
                     .build();
         }
         if(null == netAPI){
             netAPI = retrofit.create(NetAPI.class);
         }
+    }
+
+    private static void setInterceptor(OkHttpClient.Builder builder){
+        //公共参数
+        Interceptor addQueryParameterInterceptor = new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request originalRequest = chain.request();
+                Request request;
+                String method = originalRequest.method();
+                Headers headers = originalRequest.headers();
+                HttpUrl modifiedUrl = originalRequest.url().newBuilder()
+                        //在这里添加公共参数
+                        .addQueryParameter("platform", "android")
+                        .addQueryParameter("version", "1.0.0")
+                        .addQueryParameter("token","")
+                        .build();
+                request = originalRequest.newBuilder().url(modifiedUrl).build();
+                return chain.proceed(request);
+            }
+        };
+        //设置公共参数
+        builder.addInterceptor(addQueryParameterInterceptor);
+        //请求头
+        Interceptor headerInterceptor = new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request originalRequest = chain.request();
+                Request.Builder requestBuilder = originalRequest.newBuilder()
+                        .header("AppType", "TPOS")
+                        .header("Content-Type", "application/json")
+                        .header("Accept", "application/json")
+                        .method(originalRequest.method(), originalRequest.body());
+                Request request = requestBuilder.build();
+                return chain.proceed(request);
+            }
+        };
+        //设置请求头
+        builder.addInterceptor(headerInterceptor );
     }
 
     public static NetAPI getNetAPIInstance(){
